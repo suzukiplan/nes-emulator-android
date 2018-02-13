83e3b44d777ab92c664fdb468b0a21766b02675d
suzukiplan <suzukiplan.ys@gmail.com>
2018-02-14 00:34:44 +0900

    implement capture example

---
+++ test/src/main/java/com/suzukiplan/emulator/nes/test/CaptureDialog.kt
@@ -0,0 +1,26 @@
+package com.suzukiplan.emulator.nes.test
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.FragmentManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+class CaptureDialog : DialogFragment() {
+    private lateinit var capture: Bitmap
+
+    fun show(manager: FragmentManager, capture: Bitmap) {
+        this.capture = capture
+        isCancelable = true
+        show(manager, tag)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(R.layout.dialog_capture, container)
+        view?.findViewById<ImageView>(R.id.capture_preview)?.setImageBitmap(capture)
+        return view
+    }
+}
